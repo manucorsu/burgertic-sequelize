18a8b7f5466a4dba9fca8c3d973958f81f329774
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-11-08 13:49:05 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,21 @@
 import "dotenv/config";
 
 export const config = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-    ssl: true,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+  ssl: true,
 };
 
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-    process.env.DB_URL
-);
+export const sequelize = new Sequelize(process.env.DB_URL);
 
 try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
 } catch (error) {
-    console.error("Unable to connect to the database:", error);
+  console.error("Unable to connect to the database:", error);
 }
